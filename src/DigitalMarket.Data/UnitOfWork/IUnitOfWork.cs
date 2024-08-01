b3e1f29c84a964460b7ecf23dcb69f00ba325c8c
Baris Beytur <Baris.Beytur@d-teknoloji.com.tr>
2024-08-01 13:22:02 +0300

    Unit Of Work implemented.

---
+++ src/DigitalMarket.Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using DigitalMarket.Base.Entity;
+using DigitalMarket.Data.GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalMarket.Data.UnitOfWork
+{
+    public interface IUnitOfWork<TEntity> where TEntity : BaseEntity
+    {
+        Task Complete();
+        Task CompleteWithTransaction();
+        IGenericRepository<TEntity> Repository { get; }
+    }
+}
---
+++ src/DigitalMarket.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using DigitalMarket.Base.Entity;
+using DigitalMarket.Data.Context;
+using DigitalMarket.Data.GenericRepository;
+using System.Reflection.Metadata.Ecma335;
+
+namespace DigitalMarket.Data.UnitOfWork;
+
+public class UnitOfWork<TEntity> : IDisposable, IUnitOfWork<TEntity> where TEntity : BaseEntity
+{
+    private readonly DigitalMarketDbContext _context;
+
+    public UnitOfWork(DigitalMarketDbContext context)
+    {
+        _context = context;
+    }
+
+    public IGenericRepository<TEntity> Repository => new GenericRepository<TEntity>(_context);
+
+    public async Task Complete()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task CompleteWithTransaction()
+    {
+        using (var transaction = _context.Database.BeginTransaction())
+        {
+            try
+            {
+                await _context.SaveChangesAsync();
+                transaction.Commit();
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+            }
+        }
+    }
+
+    public void Dispose()
+    {
+        if (_context != null)
+        {
+            _context.Dispose();
+        }
+    }
+}   
